da9e5baf57e1e9f584a2984a1de3d41dcb5b441b
brenogomesf <bgferreira44@gmail.com>
2023-11-24 15:10:11 -0300

    fixed: crud user

--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -2,12 +2,14 @@ import { PrismaClient } from '@prisma/client';
 import { User } from 'model/user.model';
 
 export class UserService {
-  constructor(private prisma: PrismaClient) {}
+  constructor(
+    private prisma: PrismaClient
+    ) {}
 
-    create(userData: User):Promise<User | any> {
+  create(userData: User):Promise<User | any> {
     return new Promise(async (resolve, reject) => {
         try {
-            const isExist = await this.isExist(userData);
+            const isExist = await this.isExistEmail(userData);
             
               if (!isExist) {
                 const user = await this.prisma.user.create({
@@ -26,12 +28,68 @@ export class UserService {
         }
     })
   }
-  async isExist(user: User){
+  public getById(id: number): Promise<User>{
+    return new Promise(async (resolve, reject) => {
+      try {
+        const user = await this.isExistId(id)
+        resolve(user as User);
+      } catch (error:any) {
+        reject(error);
+        throw new Error(error)
+      } 
+    });
+  }
+  public update(id: number, body: User):Promise<User>{
+    return new Promise(async (resolve, reject) => {
+      try {
+        const user = await this.getById(id);
+
+        if(!user)reject("");
+  
+        const update = await this.prisma.user.update({
+          where:{
+            id: id
+          },
+          data:{
+            name: body.name,
+          }
+        })
+        resolve(update as User); 
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  public async isExistEmail(user: User){
       const isExist = await this.prisma.user.findUnique({
           where: {
             email: user.email,
           },
         });
         return isExist
+  }
+  public async isExistId(id: number){
+    const isExist = await this.prisma.user.findUnique({
+        where: {
+          id: id,
+        },
+      });
+      return isExist
+  }
+  public getAll():Promise<User[] | any> {
+      return new Promise(async (resolve, reject) => {
+          try {
+            const users = await this.prisma.user.findMany({
+              orderBy: {
+                id : 'desc'
+              }
+            });
+      
+            if(users.length <= 0) reject([]);
+            resolve(users);
+          } catch (error) {
+            reject(error)
+          }
+      })
   }
 }
